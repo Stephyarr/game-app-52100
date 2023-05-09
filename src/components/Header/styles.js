d5c455286ad8c369296b5b4c86e90c60058c1607
Stephyarr <stephaniearratia21@gmail.com>
2023-05-09 01:34:05 -0300

    commit-game

---
+++ src/components/Header/styles.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native"
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#7fb7be",
+        alignItems: "center",
+        justifyContent: "center",
+        height: 80
+    }
+});
